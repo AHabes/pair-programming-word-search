7d4fcd84a1306119cdf84a86ca3b27149a9925a9
Abdullah Habes <abdulla096@hotmail.com>
2021-11-03 17:17:35 -0400

    implement wordSearch

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,37 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function(matrix) {
+  let grid = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    grid.push([]);
+  }
+  for (let y = 0; y < matrix.length; y++) {
+    for (let x = 0; x < matrix[y].length; x++) {
+      grid[x][y] = matrix[y][x];
     }
-}
+  }
+  return grid;
+};
 
-module.exports = wordSearch
+
+const checkWordExists = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  const horizontalReverseJoin = horizontalJoin.map((ls) => ls.split("").reverse().join(""));
+
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  for (let l of horizontalReverseJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  return false;
+};
+
+const wordSearch = (letters, word) => {
+  if (letters.length === 0) return false;
+  const transposedArr = transpose(letters);
+
+  return checkWordExists(letters, word) || checkWordExists(transposedArr, word);
+};
+
+module.exports = wordSearch;
